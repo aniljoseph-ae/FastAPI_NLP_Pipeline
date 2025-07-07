e787c1681cff42f08c197e78cd006c6bfc7c3b38
aniljoseph-ae <aniljoseph.ae@gmail.com>
2025-07-07 21:09:32 +0530

    update tasks

---
+++ app/tasks/celery_tasks.py
@@ -0,0 +1,27 @@
+from celery import Celery
+
+from app.core.config import settings
+from app.services.nlp_service import NLPService
+from app.services.webhook_service import notify_webhook
+
+app = Celery(
+    "tasks", 
+    broker=settings.CELERY_BROCKER_URL, 
+    backend=settings.CELERY_RESULT_BACKEND
+)
+
+app.conf.task_serializer = "json"
+app.conf.accept_contetnt = ["json"]
+app.conf.result_serializer = "json"
+
+@app.task
+def process_nlp_task(task_id: str, input_data: str | list, task_type: str, webhook_url: str | None):
+    nlp_service = NLPService()
+    result = (
+        nlp_service.process_batch(input_data, task_type)
+        if task_type == "batch"
+        else nlp_service.process_text(input_data, task_type)
+    )
+    if webhook_url:
+        notify_webhook(webhook_url, {"task_id": task_id, "result": result})
+    return result
